1cc585ff3e7da9c95a15e6c0a6ef857311192f6c
LucioGiocondo98 <togaluc@hotmail.com>
2025-03-20 17:29:03 +0100

    update

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 const myAPI ='Jdf8MULkITM5dGwcXdS6O0MGLQRE5rAuahRGQWaHsp0OToLvhYjhIbQN'
 const urlMountain ='https://api.pexels.com/v1/search?query=mountains'
-const loadImage =  document.getElementById('buttonLoad').addEventListener('click',function(){
+const loadButton = document.getElementById('loadButton')
+const allTheDogs = document.querySelectorAll('.card img')
+const loadImage = function(){
     fetch(urlMountain,{
         headers: {
             authorization: myAPI,
@@ -9,11 +11,19 @@ const loadImage =  document.getElementById('buttonLoad').addEventListener('click
     .then((response)=>{
         if(response.ok){
             console.log('ci siamoooo',response)
+            return response.json()
         }else{
             throw new Error('errore d******')
         }
     })
+    .then((data)=>{
+        console.log('data',data)
+        allTheDogs.forEach((dogPic,i )=> {
+            dogPic.setAttribute('src',data.photos[i].src.medium)
+            
+        });
+    })
     .catch((err)=>{
         console.log('errore di ?',err)
     })
-})
+}
